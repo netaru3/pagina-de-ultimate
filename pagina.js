d4ca405d86b5620ba11a05f4a2ccde6e187d3c9f
Aarón <netaru3@gmail.com>
2025-12-13 14:42:43 -0300

    Add files via upload
    la wiki y buscador de nuestro juego ultimate ninja

---
+++ pagina.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv'
+dotenv.config()
+const apikey= process.env.API_KEY
+
+const app= express()
+app.get("/",function(req,res){
+  res.sendFile("pagina.html",{root:import.meta.dirname})
+})
+app.get("/apikey",function(req,res){
+  res.json({apikey:apikey})
+})
+
+app.get("/wiki",function(req,res){
+  res.sendFile("wiki.html",{
+    root: import.meta.dirname
+  })
+})
+
+app.listen(3000)
